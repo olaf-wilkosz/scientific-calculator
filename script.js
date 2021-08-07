7a534c4c92f99236ff2ef27b5b5349c4949dfbc9
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-08-07 12:43:34 +0200

    Add function for addition and updating result and event listener for clicking the number buttons

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const number = document.querySelectorAll('.number');
-const operator = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 const allClear = document.querySelector('.all-clear');
 const clearEntry = document.querySelector('.clear-entry');
 const equals = document.querySelector('.equals');
-const previousResult = document.querySelector('.previous-result');
-const currentResult = document.querySelector('.current-result');
+const previousActionResult = document.querySelector('.previous-result');
+const currentActionResult = document.querySelector('.current-result');
 
 let currentResult = '';
 let previousResult = '';
 let action = undefined;
+
+const updateResult = () => {
+  currentActionResult.innerText = currentResult;
+  previousActionResult.innerText = previousResult;
+};
+
+const add = (number) => {
+  currentResult = currentResult.toString() + number.toString();
+};
+
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    add(number.innerText);
+    updateResult();
+  });
+});
